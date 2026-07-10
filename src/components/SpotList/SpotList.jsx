0f11c246ae688d0037a19807d9e6fb20fe3a451e
新井　路花 <arahana@araijihananoMacBook-Air.local>
2026-07-10 15:13:51 +0900

    プラン作成の実装

--- src/components/SpotList/SpotList.jsx
+++ src/components/SpotList/SpotList.jsx
@@ -1,5 +1,6 @@
 import { Spots } from "../../data/spots";
 import { usePlanSelection } from "../../contexts/UsePlanSelection";
+import { useState } from "react";
 import { CiCirclePlus } from "react-icons/ci";
 import { FaMinusSquare } from "react-icons/fa";
 
@@ -7,11 +8,16 @@ import styles from "./SpotList.module.css";
 
 export const SpotList = () => {
 
-  const { selectedSpots, addSpot, removeSpot, clearSpot } = usePlanSelection();
+const [isFormOpen, setIsFormOpen] = useState(false);
+const [planName, setPlanName] = useState('');
+const [errormessage, setErrormessage] = useState('')
 
-  const selectedCount = selectedSpots.length;
 
-  // バリデーションと作成プラン保存
+const { selectedSpots, addSpot, removeSpot, clearSpot } = usePlanSelection();
+
+const selectedCount = selectedSpots.length;
+
+  // プラン作成ボタン押下
   const handleCreatePlan = () => {
     const count = selectedSpots.length;
 
@@ -21,17 +27,54 @@ export const SpotList = () => {
     }else if(count >= 6) {
       alert('スポットは５箇所以内で選択してください。');
       return;
+    } else {
+      setIsFormOpen(true);
+    }
+  }
+
+  // 保存ボタン押下
+  const handleSavePlan = () => {
+    const error = validatePlanName(planName);
+
+    if(error) {
+      setErrormessage(error);
+      return;
+    }
+    
+    const newPlan = {
+      id: Date.now(),
+      name: planName.trim(),  
+      spots: selectedSpots, 
+      createdAt: new Date().toISOString(),
     }
 
-    // 新しいプランを追加する
+    // 新しいプランをlocalsStorageに追加する
     const existing = JSON.parse(localStorage.getItem('createdPlans')) || [];
-    existing.push(selectedSpots);
+    existing.push(newPlan);
     localStorage.setItem('createdPlans', JSON.stringify(existing));
 
     // トーストメッセージ表示させる
-    localStorage.setItem('toastMessage', 'プランを作成しました。');
+    alert('プランを作成しました。');
 
     window.dispatchEvent(new Event('plansUpdated'));
+
+    setIsFormOpen(false);
+  }
+
+  // プラン名入力チェック
+  function validatePlanName (name) {
+    // 空白は除外
+    const trimmed = name.trim();
+
+    if(trimmed.length === 0) return 'プラン名を入力してください。';
+    if(trimmed.length > 30) return 'プラン名は30文字以内で入力してください。';
+    // エラーない場合
+    return null;
+  }
+
+  // キャンセルボタン押下
+  const handleCancelPlan = () => {
+    setIsFormOpen(false);
   }
 
   return(
@@ -76,7 +119,31 @@ export const SpotList = () => {
       <div className={styles.filterActions}>
         <button className={styles.createPlanBtn} onClick={handleCreatePlan}>プランを作成する</button>
       </div>
-      <p className={styles.memoInfo}>※スポットをクリックすると選択できます。</p>
+      <p className={styles.memoInfo}>※プラスボタンをクリックすると選択できます。</p>
+
+      {isFormOpen && (
+        <div className={styles.modalOverlay} >
+          <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+          <h3 className={styles.modalTitle}>プラン名を入力</h3>
+            <input
+            type="text"
+            className={styles.modalInput}
+            value={planName}
+            onChange={(e) => {
+              setPlanName(e.target.value);
+              if (errormessage) setErrormessage("");
+            }}
+            placeholder="プラン名を入力してください。"
+            />
+            {errormessage && <p className={styles.modalError}>{errormessage}</p>}
+
+            <div className={styles.modalActions}>
+              <button className={styles.modalCancelBtn} onClick={handleCancelPlan}>キャンセル</button>
+              <button className={styles.modalSaveBtn} onClick={handleSavePlan}>プラン保存</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
